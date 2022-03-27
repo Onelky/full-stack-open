75a083c0a4d6fc464c66253d5df35ad1e4d0c74a
onelky <onelky.hernandez29@gmail.com>
2022-03-27 13:15:40 -0400

    Add json database

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,34 +3,43 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import service from './services/persons'
+import Notification from "./components/Notification";
 
 const App = () => {
     const [persons, setPersons] = useState([]);
+    const [filter, setFilter] = useState('');
+    const [notificationMessage, setNotificationMessage] = useState(null);
+    const [notificationType, setNotificationType] = useState('');
 
     useEffect(() => {
         service.getAll()
-            .then((response) => {
-                setPersons(response);
-            }).catch((error) => {
-                console.log(error)
-            });
+            .then((response) => setPersons(response))
+            .catch((error) => console.log(error));
     }, []);
 
 
-    const [filter, setFilter] = useState('');
-
+    const showNotification = (message) => {
+        setNotificationMessage(message);
+        setTimeout(() => {
+            setNotificationMessage(null)
+        }, 5000)
+    };
     const onSubmit = (newPerson) =>{
         const existingPerson = persons.find((person) => person.name === newPerson.name);
 
         if (!existingPerson){
             service.add(newPerson)
-                .then((response) => setPersons(persons.concat(response)))
+                .then((response) => {
+                    setPersons(persons.concat(response))
+                    showNotification(`Added ${response.name}`)
+                })
                 .catch((error) => console.error(error))
         }
         else{
             service.update(existingPerson.id, newPerson)
                 .then((response) =>{
-                    setPersons(persons.map(person => person.id !== response.id ? person : response))
+                    setPersons(persons.map(person => person.id !== response.id ? person : response));
+                    showNotification(`Updated ${response.name}`)
                 })
                 .catch((error) =>{
                     console.error(error);
@@ -43,12 +52,16 @@ const App = () => {
         if(window.confirm(`Delete ${name}?`)){
             service.delete(id)
                 .then(() => setPersons(persons.filter((person) => person.id !== id)))
-                .catch((error) => console.error(error))
+                .catch(() => {
+                    setNotificationType('error');
+                    showNotification(`Information of ${name} has already been removed from server`)
+                })
         }
     }
     return (
         <div>
             <h2>Phonebook</h2>
+            {notificationMessage && <Notification type={notificationType} message={notificationMessage}/>}
             <Filter filter={filter} setFilter={setFilter}/>
             <h2>Add a new</h2>
             <PersonForm onSubmit={onSubmit}/>
